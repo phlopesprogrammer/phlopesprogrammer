30e92075ddefa1540cf6887a0a4aa2138f0b8fbf
Paulo Henrique <phlopesprogrammer@gmail.com>
2024-11-16 17:18:22 -0300

    Menu-teste
    Menu teste Faculdade

--- perfilDeUsuario/menu.js
+++
@@ -1,8 +0,0 @@
-var menuItem = document.querySelectorAll('.item-menu')
-function selectItem(){
-    menuItem.forEach((item)=>
-    item.classList.remove('ativo')
-    )
-    this.classList.add('ativo')
-    
-}   
